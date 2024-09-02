6486850e15ff72f9360fce869b4ac8572f8159f6
galloramiro <ramiro.r.gallo@gmail.com>
2024-09-02 15:38:22 -0300

    issue-2
    - Create harmonica object

---
+++ src/dto.py
@@ -0,0 +1,15 @@
+from typing import Dict
+from dataclasses import dataclass
+
+@dataclass
+class Harmonica:
+    hole_01: Dict[str, str]
+    hole_02: Dict[str, str]
+    hole_03: Dict[str, str]
+    hole_04: Dict[str, str]
+    hole_05: Dict[str, str]
+    hole_06: Dict[str, str]
+    hole_07: Dict[str, str]
+    hole_08: Dict[str, str]
+    hole_09: Dict[str, str]
+    hole_10: Dict[str, str]
